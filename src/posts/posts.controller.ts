ea949440894984e98ae08873d911327af2768591
thismajid <mkhorshidian72@gmail.com>
2022-03-19 10:31:46 +0330

    add userId for posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,18 +6,22 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Me } from 'src/auth/guards/me.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  @UseGuards(JwtAuthGuard)
+  create(@Me() me, @Body() createPostDto: CreatePostDto) {
+    return this.postsService.create({ ...createPostDto, userId: me.id });
   }
 
   @Get()
